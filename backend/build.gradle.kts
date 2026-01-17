a65b2b50ae7b4a2c9accbc6dbf20d757b308ff65
Arnaud GRIMBERT <80671351+Buco7854@users.noreply.github.com>
2026-01-17 23:47:30 +0100

    change SonarQube plugin declaration

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("java")
+    id("org.sonarqube") version "7.2.2.6593"
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    id("org.sonarqube") version "7.2.2.6593"
 }
 
 sonar {
