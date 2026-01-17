c99a37b96611a87c00717bfba90fa754bcd09199
Arnaud GRIMBERT <80671351+Buco7854@users.noreply.github.com>
2026-01-17 23:54:31 +0100

    Remove messaging security support comment
    Removed comment about messaging security support.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,7 +28,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    // Add messaging security support
     implementation("org.springframework.security:spring-security-messaging")
 
     implementation("org.postgresql:postgresql")
